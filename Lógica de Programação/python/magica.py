4cdd621af9ebf6988a7a4bce6221e20a3e2ded88
Anderson Breno <andersbreno@gmail.com>
2025-06-15 13:32:26 -0300

    Cria arquivos de estudo em JavaScript e Lógica de programação
    Avançando em JavaScript com SPA e Lógica de programação com Python.

---
+++ Lógica de Programação/python/magica.py
@@ -0,0 +1,14 @@
+import os 
+
+os.system('cls')
+print('Olhe as cartas a baixo e escolha uma: ')
+print('---')
+print('Q K J Q J K')
+input()
+
+os.system('cls')
+print('A carta que você pensou...')
+
+print()
+print('Desapareceu!')
+print('J Q J Q K K')
---
+++ Lógica de Programação/python/programa.py
@@ -0,0 +1,5 @@
+print("Hello, World!")
+print('Eu de novo')
+print(1 + 1)
+x = 15
+print(x * 2)
